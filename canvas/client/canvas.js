7f39349cbd72f19444e45058d1f62668a277924c
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-02-19 12:43:37 +0200

    Add editMode reactive variable

--- canvas/client/canvas.js
+++ canvas/client/canvas.js
@@ -1,3 +1,11 @@
+Template.leanCanvas.created = function () {
+  // Get reference to template instance
+  const instance = this;
+
+  // Add edit mode reactive variable
+  const editMode = new ReactiveVar(false);
+}
+
 Template.leanCanvas.events({
   "click #edit-canvas": function () {
     // Enable Hallo editor on all canvas text
