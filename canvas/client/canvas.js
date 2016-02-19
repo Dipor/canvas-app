a14b64bd895daf2500bcacf3ba4212b32e1cec51
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-02-19 13:02:38 +0200

    Add editMode event and helper

--- canvas/client/canvas.js
+++ canvas/client/canvas.js
@@ -3,11 +3,29 @@ Template.leanCanvas.created = function () {
   const instance = this;
 
   // Add edit mode reactive variable
-  const editMode = new ReactiveVar(false);
+  instance.editMode = new ReactiveVar(false);
 }
+
+Template.leanCanvas.helpers({
+  "editMode": function () {
+    // Keep track of whether canvas is in edit mode
+
+    // Get reference to template instance
+    const instance = Template.instance();
+
+    // Get value of edit mode reactive variable
+    return instance.editMode.get();
+  }
+});
 
 Template.leanCanvas.events({
   "click #edit-canvas": function () {
+    // Get reference to template instance
+    const instance = Template.instance();
+
+    // Set canvas to edit mode
+    instance.editMode.set(true);
+
     // Enable Hallo editor on all canvas text
     jQuery("p").hallo({
       plugins: {
